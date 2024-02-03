8f8b9c953b2177a254d0d06f16317c1c5c0f6f17
Mhashir23 <66163091+Mhashir23@users.noreply.github.com>
2024-02-03 11:12:43 +0000

    step 1

---
+++ name/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/muhammadhashir/Documents/UNI WORK/web-dev-test/name/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
